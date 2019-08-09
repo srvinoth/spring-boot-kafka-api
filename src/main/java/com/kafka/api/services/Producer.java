e943e16c3f192bbc2188fc7f7cc23ad11e014652
S.Vinoth Kumar <srvinoth@gmail.com>
2019-08-09 15:04:26 +0400

    commit for new changes

---
+++ src/main/java/com/kafka/api/services/Producer.java
@@ -0,0 +1,18 @@
+package com.kafka.api.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Producer {
+
+    private static final String TOPIC = "test";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        this.kafkaTemplate.send(TOPIC, message);
+    }
+}
